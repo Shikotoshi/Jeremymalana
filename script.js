3bc98ef4f320503e29153de78e83835d92ec7d90
Jeremy Malana <155638169+Shikotoshi@users.noreply.github.com>
2024-01-07 20:00:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".switch").click(function(){
+    $("body").toggleClass("dark-mode");
+    if($("body").hasClass("dark-mode")){
+        $(".switch").attr("name", "sunny-outline");
+    } else{
+        $(".switch").attr("name", "moon-outline");
+    }
+});
+
+AOS.init({
+    
+});
